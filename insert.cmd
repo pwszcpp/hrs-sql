95f29798ba45d844a0d65e5161ac12078c718602
merick13 <alekkam@wp.pl>
2017-12-06 10:45:01 +0100

    Update to batch script, leave file with sample data and sql export file

--- insert.cmd
+++ insert.cmd
@@ -63,6 +63,10 @@ REM Set a random field in the range of 1 to the number of fields in a current ro
 set /a rand_field=(%RANDOM%*%NF%/32768)+1
 
 if "!f[%row_number%]!"=="Pass" set rand_field=%record%
+if "!f[%row_number%]!"=="Start_date" set rand_field=%record%
+if "!f[%row_number%]!"=="End_date" set rand_field=%record%
+if "!f[%row_number%]!"=="Agreed" set rand_field=%record%
+if "!f[%row_number%]!"=="Disagree_reason" set rand_field=%record%
 
 REM Get random field from current row
 for /f "tokens=%rand_field% delims=;" %%d in ("%line%") do (
